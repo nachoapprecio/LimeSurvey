d7d82c722f676002868966edbc190e4859e329e1
nachoapprecio <imolina@apprecio.com>
2025-12-17 15:03:09 -0300

    Create symbolic links for data directories
    Add symbolic links for upload, tmp, and config directories if they do not exist.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,21 @@ set -e
 
 mkdir -p /data/upload /data/tmp /data/config
 
+if [ ! -L /var/www/html/upload ]; then
+  rm -rf /var/www/html/upload
+  ln -s /data/upload /var/www/html/upload
+fi
+
+if [ ! -L /var/www/html/tmp ]; then
+  rm -rf /var/www/html/tmp
+  ln -s /data/tmp /var/www/html/tmp
+fi
+
+if [ ! -L /var/www/html/application/config ]; then
+  rm -rf /var/www/html/application/config
+  ln -s /data/config /var/www/html/application/config
+fi
+
 chown -R www-data:www-data /data
 chmod -R 775 /data
 
